acd325486187ca90bee66022efd708d3102236cb
Ming011 <zanaya011@gmail.com>
2021-08-11 23:32:05 -0500

    completed prep js for loops

---
+++ prep-javascript-for-loops/prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+// create your loops here.
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var i = 0; i <= 18; i++) {
+  if (i % 2 === 0) console.log(i);
+}
+
+for (var i = 0; i < Infinity; i++) {
+  console.log('Time till explosion: ', i);
+}
